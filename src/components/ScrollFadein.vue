3bd2a5625754ee46836c5562b0ea0effe49efc99
きくたたくや <gtk.9223@gmail.com>
2023-09-22 17:01:15 +0900

    intersectionObserverに第二引数追加

--- src/components/ScrollFadein.vue
+++ src/components/ScrollFadein.vue
@@ -12,10 +12,15 @@ import { ref, onMounted } from "vue";
 // classの初期設定
 let blue = ref(false);
 
+let option:{rootMargin:string} = {
+  rootMargin: `0px 0px -300px 0px`
+}
+const options = ref(option);
+
 // IntersectionObserver
 const ISOSerber = new IntersectionObserver(function(e){
   e.forEach(function(endpoint){
-    console.log(endpoint.isIntersecting);//true or false
+    console.log(endpoint);//true or false
     if(endpoint.isIntersecting == false){
       console.log("見えていない");
     }else{
@@ -23,12 +28,19 @@ const ISOSerber = new IntersectionObserver(function(e){
       blue.value = true;
     }
   })
-})
+},options.value
+// {
+//   rootMargin: `0px 0px -300px 0px`
+// }
+);
 // IntersectionObserverで使う要素をrefで紐付け
 const hello = ref(); //1
+const hi = ref();
 onMounted(function() { //4
   const testDom = hello.value;  //2
   console.log(testDom);
+  console.log(hi.value.innerHTML);
+  
   // IntersectionObserverに要素を渡す
   ISOSerber.observe(testDom);
 });
